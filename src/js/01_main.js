f03d313d739fe86a689c48cf82b5382fe6558eea
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-03-10 13:36:51 +0300

    section steps

---
+++ src/js/01_main.js
@@ -0,0 +1,16 @@
+
+ 
+ function scrolHeader() {
+    const scrollWrapper = document.querySelector (".header__wrapper")
+window.addEventListener('scroll', ()=> {
+    
+    if(this.scrollY > 1) {
+        scrollWrapper.classList.add("--header__wrapper-white")
+    }
+    else (
+        scrollWrapper.classList.remove("--header__wrapper-white")
+    )
+});
+ }
+
+ scrolHeader()
